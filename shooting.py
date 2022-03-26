72ccc1f7b764b7f2217df049b566bfb5cc174823
cail626 <nathanvaneberg@gmail.com>
2022-03-26 19:01:26 +0100

    shooting

---
+++ shooting.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 14:51:42 2022
+
+@author: natha
+"""
+
+import numpy as np
+import scipy as sp
+from scipy.optimize import newton
+
+
+MISSILE_SPEED = 300
+
+def shooting(x,ennemy_pos,ennemy_speed,player_pos):
+    d = np.array([np.cos(x),np.sin(x),0])
+    
+    ve = ennemy_speed
+    vm = MISSILE_SPEED*d
+    
+    t = (ennemy_pos[0] - player_pos[0])/(vm[0]-ve[0])
+    
+    pe = ennemy_pos+t*ennemy_speed
+    pm = player_pos+t*MISSILE_SPEED*d
+    
+    return np.linalg.norm(pm-pe)
+
+def shoot_on_ennemy_prevision(gamestate):
+    player_pos = np.array(gamestate.controlled_player.position)
+    ennemy_pos = np.array(gamestate.other_players[0].position)
+    ennemy_speed  = np.array(gamestate.other_players[0].speed)
+    
+    dir0 = ennemy_pos-player_pos
+    #Calcul de l'angle de tir
+    angle0 = shoot_dir(dir0)
+    x0 = angle0
+    
+    try: 
+        res = newton(shooting,x0=x0,args=(ennemy_pos,ennemy_speed,player_pos))
+        if(shooting(res,ennemy_pos,ennemy_speed,player_pos)<0.01):
+            return res*180/np.pi
+        else:
+            return angle0*180/(np.pi)
+    except:
+        return angle0*180/(np.pi)
+    
+    
+def shoot_dir(direction):
+    
+    dx = direction[0]
+    dy = direction[1]
+    L = np.sqrt(dx**2+dy**2)
+    
+    angle0 = np.arccos(dx/L)
+    if(dy<0):
+        angle0 = 2*np.pi-angle0
+        
+    return angle0
+    
+    
+def shoot_corner(gamestate,corner):
+
+    """
+    Corner: up or down
+    """
+    
+    ennemy_pos = np.array(gamestate.other_players[0].position)
+    player_pos = np.array(gamestate.controlled_player.position)
+    x,y = ennemy_pos[0],ennemy_pos[1]
+    
+    if(corner == 'up'):
+        target_1 = np.array([0,250,0])
+        target_2 = np.array([-250,0,0])
+        if(-x>y): 
+            target = target_2
+        else:
+            target = target_1
+    else:
+        target_1 = np.array([0,-250,0])
+        target_2 = np.array([250,0,0])
+        if(-x>y):
+            target = target_1
+        else:
+            target = target_2
+            
+    return shoot_dir(ennemy_pos-player_pos)
+        
+        
+def shooting_decision(world_map,gamestate):
+    
+    player_pos = np.array(gamestate.controlled_player.position)
+    ennemy_pos = np.array(gamestate.other_players[0].position)
+    #34 = 125-91
+    if( (ennemy_pos[0] < -200 and ennemy_pos[1] > 34) or (ennemy_pos[0] < -34 and ennemy_pos[1] > 200)):
+        shoot_corner(gamestate,'up')    
+    elif( (ennemy_pos[0] > 200 and ennemy_pos[1] < -34) or (ennemy_pos[0] > 34 and ennemy_pos[1] < -200)):
+        shoot_corner(gamestate,'down')
+    else:
+        shoot_on_ennemy_prevision(gamestate)
+    
---
+++ state_machine.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 26 18:18:10 2022
+
+@author: natha
+"""
+from agent import MoveCommand, ShootCommand, Command
+
+class State_Machine:
+    
+    
+    
+    def __init__(self):
+        state = 0
+        
+    def IA(self,gamestate,my_data):
+        
+        if(self.state == 1):
+            decision = self.state_1(gamestate,my_data)
+            
+        
+        if(decision[0] == 0):
+            return MoveCommand(decision[1])
+        else:
+            return ShootCommand(decision[1])
+        
+        
+    def state_1(self,gamestate,my_data):
+        
+        #ANALYSE DATA 
+        
+        #TAKE DECISION
+        
+        #1. Evade
+        #2. Move to 
+        #3. Shoot
+        
+        
+        return 0
